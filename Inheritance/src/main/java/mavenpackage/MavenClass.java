8b3386b4e7d440e0e9facb966f42fa00b699915d
admin <admin@DESKTOP-IS3GAH4>
2023-11-16 12:40:29 +0530

    hai

--- Inheritance/src/main/java/mavenpackage/MavenClass.java
+++ Inheritance/src/main/java/mavenpackage/MavenClass.java
@@ -4,7 +4,7 @@ public class MavenClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helloo this is maven project");
+		System.out.println("Helloo this is maven project");//ghju
 
 	}
 
