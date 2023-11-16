c37c2075643f2b2b4dc940787de6f506772bba40
admin <admin@DESKTOP-IS3GAH4>
2023-11-16 12:46:54 +0530

    sum

---
+++ Inheritance/src/main/java/mavenpackage/MavenSample.java
@@ -0,0 +1,15 @@
+package mavenpackage;
+
+public class MavenSample {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		int c= a+b;
+		System.out.println(c);
+
+	}
+
+}
