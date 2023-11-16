9730d6df314b182a167992a7380561f3f0a37427
admin <admin@DESKTOP-IS3GAH4>
2023-11-16 12:37:46 +0530

    My First maven program

---
+++ Inheritance/src/main/java/mavenpackage/MavenClass.java
@@ -0,0 +1,11 @@
+package mavenpackage;
+
+public class MavenClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloo this is maven project");
+
+	}
+
+}
